1ee4a6d2c12a6e6c51d109abaa4d5ae649f605ce
Novke <novke.brate@gmail.com>
2023-07-12 21:18:05 +0200

    Napravljena klasa za serijalizaciju

--- bioskop-server/src/main/java/rs/np/ac/bg/bioskop_server/App.java
+++ bioskop-server/src/main/java/rs/np/ac/bg/bioskop_server/App.java
@@ -1,5 +1,7 @@
 package rs.np.ac.bg.bioskop_server;
 
+import rs.np.ac.bg.bioskop_server.server.Server;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Server server = new Server();
+        server.startServer();
     }
 }
---
+++ bioskop-server/src/main/java/rs/np/ac/bg/bioskop_server/serialization/Serialize.java
@@ -0,0 +1,10 @@
+package rs.np.ac.bg.bioskop_server.serialization;
+
+public class Serialize {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
